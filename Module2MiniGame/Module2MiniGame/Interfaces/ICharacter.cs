2f1f7695eec77c68ed56d3f78ac99fd282adf5ab
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-09-11 17:08:16 +0300

    Delete extra usings

--- Module2MiniGame/Module2MiniGame/Interfaces/ICharacter.cs
+++ Module2MiniGame/Module2MiniGame/Interfaces/ICharacter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Module2MiniGame.Interfaces
+﻿namespace Module2MiniGame.Interfaces
 {
     internal interface ICharacter
     {
